bafb3c17e6b6808c192bf0c64e5769e73ed88ff4
Thomas Rosendal <thomas.rosendal@sva.se>
2014-03-27 21:45:08 +0100

    changes

--- examine_diffs.r
+++ examine_diffs.r
@@ -4,20 +4,26 @@
 library(devtools)
 install_github("git2r", "ropensci")
 library(git2r)
-
 library(foreign)
 data<-data.frame('ID'=1:1000,
                  'x'= sample(seq(from=0,to=1,length.out=1000),replace=TRUE,size=1000),
                  'y'= sample(seq(from=0,to=1,length.out=1000),replace=TRUE,size=1000))
-write.dta(dataframe=data, file='points.dta')
-
-
-
+init(getwd())
 repo<-repository(getwd())
+write.dta(dataframe=data, file='points.dta')
+add(repo, 'points.dta')
+commit(repo, message='initial commit')
+commit(repo, message='initial commit')
+commit(repo, message='initial commit')
 
+#######
 newdata<-read.dta('points.dta')
+newdata$x[345]<-newdata$x[345]-0.002
+write.dta(dataframe=data, file='points.dta')
+add(repo, 'points.dta')
+commit(repo, message='made changes to data')
+######
 
-##checkout the commit prior to the changes
 checkout(commits(repo)[2])
 
 olddata<-read.dta('points.dta')
